583ffef9edf207ef880be83072a9f43f26819b44
Larry Lee <larrylee2003@live.com>
2026-01-12 23:54:33 +0800

    fix: added mitigation against Key Confusion attacks

--- backend/middleware/auth.go
+++ backend/middleware/auth.go
@@ -8,7 +8,7 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-type AuthMiddleware struct {
+type AuthMiddleware struct { // The AuthMiddleware "class" takes in the jwt key as it needs to verify authentication tokens
 	JWTKey []byte
 }
 
@@ -21,7 +21,7 @@ type contextKey string
 
 const UserIDKey contextKey = "UserID"
 
-func (m *AuthMiddleware) parseUserClaims(r *http.Request) (*Claims, error) {
+func (m *AuthMiddleware) parseUserClaims(r *http.Request) (*Claims, error) { //Takes in a cookie and returns the claims inside it
 	cookie, err := r.Cookie("token")
 	var tokenString string
 
@@ -34,17 +34,21 @@ func (m *AuthMiddleware) parseUserClaims(r *http.Request) (*Claims, error) {
 		}
 		tokenString = strings.TrimPrefix(authHeader, "Bearer ")
 	}
-	claims := &Claims{}
+	claims := &Claims{} //Create the claims structure
 	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-		return m.JWTKey, nil // Use the key stored in the struct
+		return m.JWTKey, nil // Tells the ParseWithClaims function to use the JWTkey passed to the middllware
 	})
 	if err != nil || !token.Valid {
 		return nil, err
 	}
+	//Checks the signing method, prevents Key Confusion attack
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, jwt.NoneSignatureTypeDisallowedError
+	}
 	return claims, nil
 }
 
-func (m *AuthMiddleware) ValidateToken(next http.Handler) http.Handler {
+func (m *AuthMiddleware) ValidateToken(next http.Handler) http.Handler { //validates the token and returns the userID in the context
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		claims, err := m.parseUserClaims(r)
 		if err != nil {
@@ -56,10 +60,12 @@ func (m *AuthMiddleware) ValidateToken(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
+
+// Similar to the Validate token function except it doesnt throw an error,but returns -1 (an invalid or non existent ID)
 func (m *AuthMiddleware) OptionalAuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		claims, err := m.parseUserClaims(r)
-		var userID int64 = 0
+		var userID int64 = -1
 		if err == nil {
 			userID = claims.UserID
 		}
