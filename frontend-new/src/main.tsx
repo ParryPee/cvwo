54c9c3a28a5d8513fe89497401fa3b0addeea2f9
Larry Lee <larrylee2003@live.com>
2026-01-20 00:11:52 +0800

    chore: removed unecessary imports

--- frontend-new/src/main.tsx
+++ frontend-new/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./app";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
-import { StrictMode } from "react";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement,
